82542d83a82c3e3e38227e9027e5d9f182cdefe1
razhang <raymondzhang@Objectivasoftware.com>
2015-12-24 17:50:39 +0800

    scripts

--- client_up.sh
+++ client_up.sh
@@ -6,10 +6,7 @@ ip addr add $net dev $intf
 ip link set $intf mtu $mtu
 ip link set $intf up
 
-sudo sysctl -w net.ipv4.ip_forward=1
-sudo iptables -t nat -A POSTROUTING -s $net -o eth0 -j MASQUERADE
-sudo iptables -A FORWARD -p tcp --syn -s $net -j TCPMSS --set-mss $mtu
 
-sudo ip route add $server via $(ip route show 0/0 | sed -e 's/.* via \([^ ]*\).*/\1/')
-ip route add   0/1 dev tun
-ip route add 128/1 dev tun
+sudo ip route add $server dev eth0
+ip route add   0/1 dev intf
+ip route add 128/1 dev intf
