cc7e3c42754685dc9fb9df7942362dead555ba46
objectmarshaler <objectmarshaler@gmail.com>
2016-01-06 08:51:29 -0500

    script

--- client_up.sh
+++ client_up.sh
@@ -1,6 +1,6 @@
 intf=tun0
 mtu=1432
-server=10.32.193.94
+server=192.168.1.100
 net=10.0.0.1/8
 ip addr add $net dev $intf
 ip link set $intf mtu $mtu
@@ -8,5 +8,5 @@ ip link set $intf up
 
 
 sudo ip route add $server dev eth0
-ip route add   0/1 dev intf
-ip route add 128/1 dev intf
+ip route add   0/1 dev $intf
+ip route add 128/1 dev $intf
--- server_up.sh
+++ server_up.sh
@@ -1,12 +1,15 @@
-intf=tun0
+intf=tun2
 mtu=1432
+gw_intf=eth0
+net=10.1.1.1/24
+#ip addr add $net dev $intf
+#ip link set $intf mtu $mtu
+#ip link set $intf up
 
-net=10.0.0.1/8
-ip addr add $net dev $intf
-ip link set $intf mtu $mtu
-ip link set $intf up
-
-sudo sysctl -w net.ipv4.ip_forward=1
-sudo iptables -t nat -A POSTROUTING -s $net -o eth0 -j MASQUERADE
-sudo iptables -A FORWARD -p tcp --syn -s $net -j TCPMSS --set-mss $mtu
+sysctl -w net.ipv4.ip_forward=1
+if !(iptables-save -t nat | grep -q "$gw_intf (shadowvpn)"); then
+  iptables -t nat -A POSTROUTING -o $gw_intf -m comment --comment "$gw_intf (shadowvpn)" -j MASQUERADE
+fi
+iptables -A FORWARD -i $gw_intf -o $intf -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -i $intf -o $gw_intf -j ACCEPT
 
